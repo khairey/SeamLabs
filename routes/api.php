e1272b701ecb3f8d065b8237d2d8a92c6a2eb686
ahmed khairey <ahmed_khairey@yahoo.com>
2023-01-04 12:52:20 +0200

    User Auth

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller('\App\Http\Controllers\Api\AuthController')->group(function () {
+    Route::post('register', 'register'); // Signup
+    Route::post('login', 'login'); // login
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', '\App\Http\Controllers\Api\AuthController@logout'); //logout
+});
